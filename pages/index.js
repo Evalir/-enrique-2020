e669b3c85427d94969c1336724ee98ac2755cc39
Enrique Ortiz <hi@enriqueortiz.dev>
2021-12-16 01:07:09 +0100

    chore: fix compiler warnings

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,10 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Enrique Ortiz</title>
-        <meta name="description" content="Enrique Ortiz's personal site" />
+        <meta name="description" content="Enrique Ortiz&apos;s personal site" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap"
           rel="stylesheet"
@@ -28,7 +28,7 @@ export default function Home() {
           />
         </div>
         <p className={styles.description}>
-          My name is Enrique Ortiz. I'm a software developer from the Dominican
+          My name is Enrique Ortiz. I&apos;m a software developer from the Dominican
           Republic, an island in the middle of the Caribbean Sea.
         </p>
         <p className={styles.description}>
